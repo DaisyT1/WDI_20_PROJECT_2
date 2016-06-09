8868edf7bda6378679bac05b6d53a48735ec3aaa
Daisy Tucker <daisyatucker@gmail.com>
2016-06-10 00:47:05 +0100

    Delete page redirect bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -62,7 +62,7 @@ class PagesController < ApplicationController
   def destroy
     @page.destroy
     respond_to do |format|
-      format.html { redirect_to pages_url, notice: 'Page was successfully destroyed.' }
+      format.html { redirect_to edit_story_path(@page.story), notice: 'Page was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
